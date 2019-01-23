56826d847e702bece792d3cad3a6512040e3621a
Arthur Lam <lam.arthur0311@gmail.com>
2019-01-23 01:09:30 +0000

    implemented renderTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,54 +5,87 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
     },
-    "handle": "@SirIsaac"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
 
 $(document).ready(function() {
 
-function createTweetElement(tweet) {
-  let $tweet = `
-    <article class="tweet">
-      <header class="tweet-header">
-        <nav class="nav-bar">
-            <img class="avatar" src="${tweet.user.avatars.regular}">
-            <span class="name">${tweet.user.name}</span>
-            <span class="username">${tweet.user.handle}</span>
-        </nav>
-      </header>
-      <p class="tweet-body">${tweet.user.handle}</p>
-      <footer class="tweet-footer">
-            <span class="day-posted">10 days ago</span>
-            <i class="fas fa-heart"></i>
-            <i class="fas fa-retweet"></i>
-            <i class="fas fa-flag"></i>
-      </footer>
-    </article>
-  `;
+  function renderTweets(tweets) {
+    // loops through tweets
+    tweets.forEach(function (tweet) {
+      // calls createTweetElement for each tweet
+      $('.tweet-container').append(createTweetElement(tweet));
+    });
+  }
 
-  return $tweet;
-}
+  function createTweetElement(tweet) {
+    // putting down the html template and return as template string
+    let $tweet = `
+      <article class="tweet">
+        <header class="tweet-header">
+          <nav class="nav-bar">
+              <img class="avatar" src="${tweet.user.avatars.regular}">
+              <span class="name">${tweet.user.name}</span>
+              <span class="username">${tweet.user.handle}</span>
+          </nav>
+        </header>
+        <p class="tweet-body">${tweet.user.handle}</p>
+        <footer class="tweet-footer">
+              <span class="day-posted">10 days ago</span>
+              <i class="fas fa-heart"></i>
+              <i class="fas fa-retweet"></i>
+              <i class="fas fa-flag"></i>
+        </footer>
+      </article>
+    `;
+    return $tweet;
+  }
 
-let $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-$('.tweet-container').append($tweet);
-
-console.log($tweet); // to see what it looks like
-
+  renderTweets(data);
 });
 
 

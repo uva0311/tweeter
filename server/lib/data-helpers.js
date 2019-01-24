f333d3ce9a7bf8bfad411da442f5034ab7dc62b9
Arthur Lam <lam.arthur0311@gmail.com>
2019-01-24 21:07:44 +0000

    Modified getTweet() based on connection with mongoDB

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,41 +1,5 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
-const MongoClient = require("mongodb").MongoClient;
-const MONGODB_URI = "mongodb://localhost:27017/tweeter";
-
-MongoClient.connect(MONGODB_URI, (err, db) => {
-  if (err) {
-    console.error(`Failed to connect: ${MONGODB_URI}`);
-    throw err;
-  }
-
-  // We have a connection to the "tweeter" db, starting here.
-  console.log(`Connected to mongodb: ${MONGODB_URI}`);
-
-  // ==> Refactored and wrapped as new, tweet-specific function:
-  function getTweets(callback) {
-    db.collection("tweeter").find().toArray(callback);
-  }
-
-  // ==> Later it can be invoked. Remember even if you pass
-  //     `getTweets` to another scope, it still has closure over
-  //     `db`, so it will still work. Yay!
-
-  getTweets((err, tweets) => {
-    if (err) throw err;
-
-    console.log("Logging each tweet:");
-    for (let tweet of tweets) {
-      console.log(tweet);
-    }
-
-    db.close();
-  });
-
-});
-
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
@@ -50,11 +14,7 @@ module.exports = function makeDataHelpers(db) {
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+      db.collection("tweeter").find().toArray(callback);
     }
-
   };
 }

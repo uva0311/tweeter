118ab2d510eecf4bf88c6736f12d990556040b2f
Arthur Lam <lam.arthur0311@gmail.com>
2019-01-24 21:34:42 +0000

    modified saveTweet() based on connection with mongoDB

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -6,10 +6,8 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      db.collection("tweeter").insertOne(newTweet);
+      callback(null, true);
     },
 
     // Get all tweets in `db`, sorted by newest first
